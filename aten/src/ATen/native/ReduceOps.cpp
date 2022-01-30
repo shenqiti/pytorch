aa0d82b237e51cb4b202feef9c4b326a12daab23
Ansley Ussery <ansley@fb.com>
2022-01-30 16:58:21 +0000

    Port `amax` to structured kernel

--- aten/src/ATen/native/ReduceOps.cpp
+++ aten/src/ATen/native/ReduceOps.cpp
@@ -266,6 +266,20 @@ TORCH_META_FUNC(aminmax)
   this->set_output(1, shape, options);
 }
 
+TORCH_META_FUNC(amax)
+(const Tensor& self, IntArrayRef dims, bool keepdim) {
+  auto maybe_result = maybe_get_output();
+  if (maybe_result.defined()) {
+    TORCH_CHECK(self.scalar_type() == maybe_result.scalar_type(), "Expected the dtype for input and out to match, but got ",
+            self.scalar_type(), " for input's dtype and ",  maybe_result.scalar_type(), " for out's dtype.");
+  }
+  if (self.numel() == 0) {
+    at::native::zero_numel_check_dims(self, dims, "amax()");
+  }
+  const auto& out_dtype = get_result_or_self_value_dtype(self, maybe_result, c10::nullopt);
+  resize_reduction(*this, self, dims, keepdim, out_dtype);
+}
+
 } // namespace meta
 
 namespace native {
@@ -1434,23 +1448,13 @@ Tensor amin(const Tensor& self, IntArrayRef dim, bool keepdim) {
   return at::amin_out(result, self, dim, keepdim);
 }
 
-Tensor &amax_out(const Tensor& self, IntArrayRef dim, bool keepdim, Tensor& result) {
-  TORCH_CHECK(self.scalar_type() == result.scalar_type(), "Expected the dtype for input and out to match, but got ",
-              self.scalar_type(), " for input's dtype and ",  result.scalar_type(), " for out's dtype.");
-  if (self.numel() == 0) {
-    zero_numel_check_dims(self, dim, "amax()");
-  }
-
-  auto iter = make_reduction("amax", result, self, dim, keepdim, self.scalar_type());
+TORCH_IMPL_FUNC(amax_out) (const Tensor& self, IntArrayRef dim, bool keepdim, const Tensor& result) {
+  c10::MaybeOwned<Tensor> in = c10::MaybeOwned<Tensor>::borrowed(self);
+  auto iter =
+      meta::make_reduction(*in, result, dim, keepdim, self.scalar_type());
   if (iter.numel() != 0) {
     max_values_stub(iter.device_type(), iter);
   }
-  return result;
-}
-
-Tensor amax(const Tensor& self, IntArrayRef dim, bool keepdim) {
-  Tensor result = at::empty({0}, self.options());
-  return at::amax_out(result, self, dim, keepdim);
 }
 
 template <class Stub>
